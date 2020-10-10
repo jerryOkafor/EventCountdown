21316c2b13aa379f3bbac5c90038d5c0cf07a684
Jerry Hanks <jerryhanksokafor@gmail.com>
2020-10-10 19:49:15 +0100

    add : DANGER_GITHUB_API_TOKEN, switch danger to run in macos-latest

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,4 +1,7 @@
 import Danger 
 let danger = Danger()
 
+let editedFiles  = danger.git.modifiedFiles + danger.git.createdFiles
+let deletedFiles = danger.git.deletedFiles
+
 danger.message("Hello from Danger")
